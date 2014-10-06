5b05417c6a85612da536b539776ca806981c2966
Arthur Lockman <hello@rthr.me>
2014-10-06 00:18:33 -0400

    Complete! Still untested though...

--- Homework_6.ino
+++ Homework_6.ino
@@ -2,32 +2,30 @@
 #include <TimerOne.h>
 #include <stdio.h>
 
-LiquidCrystal m_lcd(12, 11, 10, 9, 8, 7);
+LiquidCrystal m_lcd(12, 11, 10, 9, 8, 7); //Instantiate LCD
 
-static const int kIntegrationPeriodSelect = 3;
-static const int kSignal = 0;
-static const unsigned long kTimerPeriod = 500000;
+static const int kIntegrationPeriodSelect = 3; //Pin to select integration period
+static const int kSignal = 0; //Signal interrupt pin
+static const unsigned long kTimerPeriod = 500000; //Timer period
 
-volatile int counter = 0; //number of 
+volatile int counter = 0; //number of interrupts since last reset
 volatile int integrationPeriod = 1; //2 for low, 1 for high
-volatile float lastFrequency = 0.0;
-volatile unsigned long numberOfPulses = 0;
+volatile float lastFrequency = 0.0; //last read frequency
+volatile unsigned long numberOfPulses = 0; //number of pulses since last integration
 
 void setup()
 {
-	Serial.begin(115200);
-	m_lcd.begin(16, 2);
-	Timer1.initialize(kTimerPeriod);
-	Timer1.attachInterrupt(integrateSignal);
-	pinMode(kIntegrationPeriodSelect, INPUT_PULLUP);
-	pinMode(2, INPUT_PULLUP);
-	attachInterrupt(kSignal, countPulse, FALLING);
+	m_lcd.begin(16, 2); //begin LCD
+	Timer1.initialize(kTimerPeriod); //initialze timer interrupt
+	Timer1.attachInterrupt(integrateSignal); //attach timer interrupt
+	pinMode(kIntegrationPeriodSelect, INPUT_PULLUP); //setup integration period select pin
+	attachInterrupt(kSignal, countPulse, FALLING); //setup signal interrupt.
 }
 
 void loop()
 {
 	(digitalRead(kIntegrationPeriodSelect) == 1)? integrationPeriod = 1 : integrationPeriod = 2;
-	switch(integrationPeriod)
+	switch(integrationPeriod) //Change LCD display to reflect selected integration period.
 	{
 		case 1:
 			m_lcd.setCursor(0, 0);
@@ -38,53 +36,47 @@ void loop()
 			m_lcd.print("Range: low ");
 			break;
 	}
-	float frequency = lastFrequency;
-	m_lcd.setCursor(0, 1);
-	char speedOutput[16];
-	unsigned long temp = (frequency - (unsigned long)frequency) * 100;
-	sprintf(speedOutput, "%d.%d Hz   ", (unsigned long)frequency, temp);
-	m_lcd.print(speedOutput);
-}
-
-void integrateSignal()
-{
-	noInterrupts();
-	int period = counter % integrationPeriod;
-	if (period == 0)
-	{
-		counter = 0;
-		lastFrequency = (float)numberOfPulses / (0.5 * (float)integrationPeriod);
-		numberOfPulses = 0;
-	}
-	counter++;
-	interrupts();
-}
-
-void countPulse()
-{
-	numberOfPulses++;
+	float frequency = lastFrequency; //Set local frequency to last calculated frequency.
+	m_lcd.setCursor(0, 1); //Set cursor to proper position.
+	char speedOutput[16]; //Create character buffer to hold speed.
+	unsigned long decimals = (frequency - (unsigned long)frequency) * 100; //decimals in frequency
+	snprintf(speedOutput, 16, "%d.%d Hz   ", (unsigned long)frequency, decimals); //create output string
+	m_lcd.print(speedOutput); //write output string to LCD.
 }
 
 /**
- * @brief Get number of digits in number.
- * @details Get the number of digits in an 
- * integer. This is needed to add padding 
- * to the numbers in the clock.
- * 
- * @param number The number to size.
- * @return The number of digits in number.
+ * @brief Determine signal speed.
+ * @details Integrates the number of pulses over
+ * the given time period.
  */
-int digits(int number)
+void integrateSignal()
 {
-	if (number == 0) return 1; //0 has one digit
-	else
+	noInterrupts(); //disable interrupts
+	/**
+	 * What this does is determine if the code should be run based on the selected
+	 * integration period. The timer interrupt runs every .5 seconds. If the high period
+	 * is selected then integrationPeriod == 1, so the code runs every interrupt. If 
+	 * low is selected then integrationPeriod == 2, so the code runs every other 
+	 * interrupt.
+	 */
+	int period = counter % integrationPeriod;
+	if (period == 0)
 	{
-		int digits = 0;
-		while (number) //divide by 10 until number is 0
-		{
-			number /= 10;
-			digits++;
-		}
-		return digits; //return number of digits
+		counter = 0; //Reset interrupt counter.
+		lastFrequency = (float)numberOfPulses / (0.5 * (float)integrationPeriod); //Determine frequency.
+		numberOfPulses = 0; //Reset pulse counter.
 	}
+	counter++; //increment interrupt counter.
+	interrupts(); //re-enable interrupts.
+}
+
+/**
+ * @brief Counts a pulse.
+ * @details All this method does is increment
+ * the pulse counter, indicating that a pulse has
+ * occurred on the signal line.
+ */
+void countPulse()
+{
+	numberOfPulses++;
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+RBE 2001 Homeowrk 6
+---
+
+This homework assignment tasked us with creating an Arduino program that would use the Arduino to measure the speed (in Hz) of an incoming signal. We did this using an Arduino Uno and a standard LCD display.
